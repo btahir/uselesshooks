81213464929c3b0b98e4191d7a0df4e7bd7098b5
btahir89 <bilal.tahir89@gmail.com>
2022-12-17 22:16:35 -0800

    add value

---
+++ hooks/useValue.jsx
@@ -0,0 +1,7 @@
+// This hook (only cause it starts with the word use...) returns the value that it is passed. 
+// Cause why not.
+function useValue(initialValue) {
+  return initialValue
+}
+
+export default useValue
